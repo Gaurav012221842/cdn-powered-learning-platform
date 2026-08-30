84365a67d26a74197332be873be2dc1f35e6e679
Gaurav Kumar <gaurav94174@gmail.com>
2026-08-31 00:40:58 +0530

    Enable @EnableAsync and configure 3s mail timeouts for instant OTP response

--- LearningPlatformApplication/src/main/java/com/LearningPlatformApplication/LearningPlatformApplication.java
+++ LearningPlatformApplication/src/main/java/com/LearningPlatformApplication/LearningPlatformApplication.java
@@ -2,7 +2,9 @@ package com.LearningPlatformApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication(exclude = {
 		org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration.class
 })
